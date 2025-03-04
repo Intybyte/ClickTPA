dfce1f944d7ac5fcc1c7829151df459693ef38ea
Intybyte <vaan.testwork@gmail.com>
2025-03-04 16:47:29 +0100

    Add teleport to TpaUser

--- common/src/main/java/me/vaan/clickTpa/common/TpaUser.java
+++ common/src/main/java/me/vaan/clickTpa/common/TpaUser.java
@@ -1,9 +1,11 @@
 package me.vaan.clickTpa.common;
 
+import me.vaan.clickTpa.common.enums.TeleportType;
 import net.kyori.adventure.audience.Audience;
 
 public interface TpaUser {
     Audience audience();
     String name();
     boolean isOnline();
+    void teleport(TpaUser target, TeleportType type);
 }
